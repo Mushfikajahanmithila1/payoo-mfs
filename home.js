48d0f8e33ae9770ae5d006f3af242bbd4008efa8
mushfika <musfika962@gmail.com>
2026-05-13 19:31:44 +0600

    added payoo

--- home.js
+++ home.js
@@ -5,8 +5,24 @@ document
     event.preventDefault(); // Prevent form submission
     // console.log("Add money button clicked");
     const amountInput = document.getElementById("in-add-money").value;
-    console.log("Amount to add:", amountInput);
+    // console.log("Amount to add:", amountInput);
     const pinInput = document.getElementById("in-pin").value;
-    console.log("Pin entered:", pinInput);
+    // console.log("Pin entered:", pinInput);
     // Here you can add your logic to handle the add money action, e.g., send data to the server
+
+    if (pinInput === "1234") {
+      // console.log("Pin is correct. Proceeding to add money.");
+
+      const amountElement = document.getElementById("amount").innerText;
+      // console.log("Current amount element:", amountElement);
+
+      const addMoneynumber = parseFloat(amountInput);
+      // console.log(addMoneynumber);
+      const currentAmount = parseFloat(amountElement);
+      const newBlance = currentAmount + addMoneynumber;
+      console.log("New balance:", newBlance);
+      document.getElementById("amount").innerText = newBlance;
+    } else {
+      alert("Incorrect pin. Cannot add money.");
+    }
   });
